35c625173455fd72c22a5a0951ab8d36f00ad6bf
Juan <juanbello1607@gmail.com>
2024-02-29 14:41:54 -0400

    4.4

--- Part 3/blogList/utils/list_helper.js
+++ Part 3/blogList/utils/list_helper.js
@@ -1,7 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (acc, curr) => curr['likes'] + acc, 0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
