be75938265c473c32df821e0304bfd08997a551d
Juan <juanbello1607@gmail.com>
2024-03-11 15:49:50 -0400

    4.23

--- Part 3/blogList/tests/api.test.js
+++ Part 3/blogList/tests/api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach, describe } = require('node:test')
+const { test, after, before, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -7,6 +7,8 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const initialState = [
   {
@@ -70,19 +72,33 @@ const initialState = [
     likes: 21
   }
 ]
-// eslint-disable-next-line no-unused-vars
-const usersInitialState = []
+
+before(async () => {
+  const testUser = {
+    username: 'root',
+    name: 'root',
+    passwordHash: await bcrypt.hash('12356789', 10)
+  }
+  const user = new User(testUser)
+  await user.save()
+})
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  await User.deleteMany({})
+  const user = await User.findOne({ username: 'root' })
 
-  await Blog.insertMany(initialState)
+  await Blog.insertMany(initialState.map((actual) => ({
+    ...actual,
+    user: user._id
+  })))
 })
 
 test('all notes are returned as JSON', async () => {
+  const token = await getTestToken()
+
   const response = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
@@ -90,31 +106,37 @@ test('all notes are returned as JSON', async () => {
 })
 
 test('the unique identifier property of the blog posts is named id', async () => {
+  const token = await getTestToken()
   const response = await api.get('/api/blogs')
+    .set('Authorization', token)
 
   assert(Object.hasOwn(response.body[0], 'id'))
 })
 
-test('Makin a HTTP POST Request, succesfully creates a new blog post', async () => {
+test('Making a HTTP POST Request, succesfully creates a new blog post', async () => {
   const newBlogPost = {
     title: 'Rust is Awesome',
     author: 'Rustecean',
     url: 'www.rustlang.org',
     likes: 200
   }
-
+  const token = await getTestToken()
   const response = await api.post('/api/blogs')
     .send(newBlogPost)
+    .set('Authorization', token)
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
   const allData = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
 
   delete response.body.id
 
+  const user = await User.findOne({ username: 'root' })
+
   assert.strictEqual(allData.body.length, initialState.length + 1)
-  assert.deepStrictEqual(response.body, newBlogPost)
+  assert.deepStrictEqual(response.body, { ...newBlogPost, user: user._id.toString() })
 
 })
 
@@ -125,8 +147,11 @@ test('if likes property is missing, it will be default to 0', async () => {
     url: 'www.rustlang.org',
   }
 
+  const token = await getTestToken()
+
   const response = await api.post('/api/blogs')
     .send(newBlogPost)
+    .set('Authorization', token)
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
@@ -139,28 +164,37 @@ test('if the title or url properties are missing, responds to the request with t
     likes: 200
   }
 
+  const token = await getTestToken()
+
   await api.post('/api/blogs')
     .send(newBlogPost)
+    .set('Authorization', token)
     .expect(400)
 })
 
 test('delete succesfully a single blog', async () => {
+  const token = await getTestToken()
   let allNotes = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
 
   let singleNoteId = allNotes.body[0].id
 
   await api.delete(`/api/blogs/${singleNoteId}`)
+    .set('Authorization', token)
     .expect(204)
 
   let allNotesBeforeDelete = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
 
   assert.strictEqual(allNotesBeforeDelete.body.length, allNotes.body.length - 1)
 })
 
 test('update succesfully a single blog', async () => {
+  const token = await getTestToken()
   let allNotes = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
 
   let singleNoteId = allNotes.body[0].id
@@ -174,17 +208,36 @@ test('update succesfully a single blog', async () => {
 
   let putResponse = await api.put(`/api/blogs/${singleNoteId}`)
     .send(newNote)
+    .set('Authorization', token)
     .expect(200)
 
   let newState = await api.get('/api/blogs')
+    .set('Authorization', token)
     .expect(200)
+
+  delete putResponse.body.user
+  delete newState.body[0].user
 
   assert.strictEqual(putResponse.body.id, singleNoteId)
   assert.deepStrictEqual(putResponse.body, newState.body[0])
 })
 
-describe.only('Users Test', () => {
-  test.only('create succesfully a single user', async () => {
+test.only('Creating a user without a token return 401', async () => {
+  const newBlogPost = {
+    title: 'Rust is Awesome',
+    author: 'Rustecean',
+    url: 'www.rustlang.org',
+    likes: 200
+  }
+
+  await api.post('/api/blogs')
+    .send(newBlogPost)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
+})
+
+describe('Users Test', () => {
+  test('create succesfully a single user', async () => {
     let newUser = {
       username: 'foooo',
       name: 'foo bar',
@@ -193,12 +246,12 @@ describe.only('Users Test', () => {
 
     let response = await api.post('/api/users')
       .send(newUser)
-      //.expect(201)
+    //.expect(201)
     console.log(response.body)
     assert(Object.hasOwn(response.body, 'id'))
   })
 
-  test.only('cant create a user with a password or username shorter than 3 character', async () => {
+  test('cant create a user with a password or username shorter than 3 character', async () => {
     let badPassword = {
       username: 'foooo',
       name: 'foo bar',
@@ -228,5 +281,19 @@ describe.only('Users Test', () => {
 })
 
 after(async () => {
+  await User.deleteMany({})
   await mongoose.connection.close()
 })
+
+const getTestToken = async () => {
+  let user = await User.findOne({ username: 'root' })
+
+  let unsignedToken = {
+    username: 'root',
+    id: user._id
+  }
+
+  let token = jwt.sign(unsignedToken, process.env.SECRET, { expiresIn: 60 * 60 })
+
+  return `Bearer ${token}`
+}
